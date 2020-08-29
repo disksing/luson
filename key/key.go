14bf078275507c92fa695f2d375635fe6a808565
disksing <i@disksing.com>
2020-08-29 21:14:43 +0800

    api key load and generate

---
+++ key/key.go
@@ -0,0 +1,38 @@
+package key
+
+import (
+	"io/ioutil"
+	"os"
+	"path"
+
+	"github.com/disksing/luson/config"
+	uuid "github.com/satori/go.uuid"
+	"go.uber.org/zap"
+)
+
+type APIKey string
+
+func NewAPIKey(dataDir config.DataDir, logger *zap.SugaredLogger) (APIKey, error) {
+	defer logger.Sync()
+	f := path.Join(string(dataDir), fname)
+	data, err := ioutil.ReadFile(f)
+	if os.IsNotExist(err) {
+		logger.Infof("%s not exist, creating", f)
+		k := uuid.NewV4().String()
+		err = ioutil.WriteFile(f, []byte(k), 0600)
+		if err != nil {
+			logger.Errorw("failed to persist api key", zap.Error(err))
+			return "", err
+		}
+		logger.Infof("api key saved to %s", f)
+		return APIKey(k), nil
+	}
+	if err != nil {
+		logger.Errorw("failed to open api key file", zap.Error(err))
+		return "", err
+	}
+	logger.Info("api key loaded")
+	return APIKey(data), nil
+}
+
+const fname = "api-key"
